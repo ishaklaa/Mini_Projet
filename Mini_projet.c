721c9c9141151e240e43bd58df0118da4ee2bd30
ishak laaouafi <laaouafi6@gmail.com>
2025-09-08 15:10:42 +0200

    porjet1

--- Mini_projet.c
+++ Mini_projet.c
@@ -1,43 +1,84 @@
 #include <stdio.h>
 
 int main(void){
-    float T[10],moy,S,max,min;
-    int N,i,j;
+    float T[100],moy,S,max,min, v,w, moy2,moy3;
+    int N,i,j,pos,choix;
     
-    printf("entrer la note\n");
+    printf("combien des notes:");
+    scanf("%d",&j);
     
-    for(i=0;i<=9;i++){
+    for(i=0;i<j;i++){
         printf("note %d:",i+1);
         scanf("%f",&T[i]);
         
     }
     S=0;
-    for(i=0;i<=9;++i){
+    for(i=0;i<=j;i++){
         S=S+T[i];
-        moy=S/20;
+        moy=S/j;
         
     }
+    moy=S/j;
+        
     
     max=T[0];
-    for(i=0;i<=9;i++){
+    for(i=0;i<j;i++){
         if(max<T[i])
         max=T[i];
     }
     min=T[0];
-    for(i=1;i<=9;i++){
+    for(i=1;i<j;i++){
         if(min>T[i])
         min=T[i];
     }
-    printf("%.2f\n",moy);
+    printf("le moyenne: %.2f\n",moy);
     printf("la note max :%.2f\n",max);
     printf("la note min :%.2f\n",min);
     if(moy>10){
-        printf("admis");
+        printf("admis\n");
     }
-    else printf("non admis");
-
-
+    else printf("non admis\n");
+    printf("1--modifier une note: \n");
+    printf("2--suprimer une note:\n");
+    scanf("%d",&choix);
+    switch (choix)
+    {
+        case 1: printf("quelle note voulez vous modifier?\n");
+                scanf("%d",&N);
+                printf("entrer la nouvelle note\n");
+                scanf("%f",&v);
+                T[N-1]=v;
+                S=0;
+                for(i=0;i<=j;i++){
+                S=S+T[i];}
+                moy2=S/j;
+                printf("le moyenne: %.2f\n",moy2);
+                break;
+        case 2: do {printf("quelle note vous voulez suprimer?\n");
+                scanf("%d",&pos);
+                }
+                while((pos<=0 || pos>j));
+                for (i=pos;i<j;i++)
+                {
+                T[i]=T[i+1];
+                }
+                T[pos-1]=T[i];
+                j--;
+                S=0;
+                for(i=0;i<=j;i++){
+                S=S+T[i];
+                }
+                moy3=S/(j);
+                printf("le moyenne: %.2f\n",moy3);
+                break;
+                }
    
+
+    
+    
+    
+    
+
     return 0;
 
 }
